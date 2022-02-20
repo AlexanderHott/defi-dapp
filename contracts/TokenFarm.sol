80c9b85b441d898d805615f1807e07b9f667696a
AlexanderHOtt <aott01@protonmail.ch>
2022-02-20 00:24:53 -0800

    finish deploy script

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -3,36 +3,43 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract TokenFarm is Ownable {
     // Token address => (staker address => amount)
     mapping(address => mapping(address => uint256)) public stakingBalance;
     mapping(address => uint256) public uniqueTokensStaked;
+    mapping(address => address) public tokenToPriceFeed;
     address[] public stakers;
     address[] public allowedTokens;
     IERC20 public dappToken;
 
-    constructor(address _dappTokenAddress) public {
+    constructor(address _dappTokenAddress) {
         dappToken = IERC20(_dappTokenAddress);
     }
 
-    /// @dev Give out DAPP tokens based on the amount of staked tokens
+    /// @dev Set a price feed address for a token.
+    /// @param _token The address of the token.
+    /// @param _priceFeed The address of the price feed.
+    function setPriceFeedContract(address _token, address _priceFeed)
+        public
+        onlyOwner
+    {
+        tokenToPriceFeed[_token] = _priceFeed;
+    }
+
+    /// @dev Give out DAPP tokens based on the amount of staked tokens to all users.
     function issueTokens() public onlyOwner {
         for (uint256 i = 0; i < stakers.length; i++) {
             address staker = stakers[i];
             uint256 staked = getUserTotalValue(staker);
-            dappToken.transfer(staker);
-
-            uint256 tokens = staked * uniqueTokensStaked[msg.sender];
-            if (tokens > 0) {
-                allowedTokens[msg.sender].transfer(staker, tokens);
-            }
+            dappToken.transfer(staker, staked);
         }
     }
 
-    /// @dev Add tokens to stake in this contract
-    /// @param _tokenAddress Address of the token to stake
-    /// @param _amount Amount of tokens to stake
+    /// @dev Add tokens to stake in this contract.
+    /// @param _amount Amount of tokens to stake.
+    /// @param _token Address of the token to stake.
     function stakeTokens(uint256 _amount, address _token) public payable {
         require(_amount > 0, "Amount must be more than 0.");
         require(tokenIsAllowed(_token), "Token is not allowed.");
@@ -42,7 +49,7 @@ contract TokenFarm is Ownable {
         );
 
         // Transfer funds
-        ERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
 
         // Update internal state
         stakingBalance[_token][msg.sender] =
@@ -55,9 +62,19 @@ contract TokenFarm is Ownable {
         }
     }
 
-    /// @dev Checks whether a token is allowed to be staked
-    /// @param _token The token to check
-    /// @return Whether the token is allowed to be staked
+    /// @dev Remove tokens from staking and transfer them to the caller.
+    /// @param _token Address of the token to unstake.
+    function unstakeTokens(address _token) public {
+        uint256 balance = stakingBalance[_token][msg.sender];
+        require(balance > 0, "No tokens to unstake.");
+        IERC20(_token).transfer(msg.sender, balance);
+        stakingBalance[_token][msg.sender] = 0;
+        uniqueTokensStaked[msg.sender] = uniqueTokensStaked[msg.sender] - 1;
+    }
+
+    /// @dev Checks whether a token is allowed to be staked.
+    /// @param _token The token to check.
+    /// @return Whether the token is allowed to be staked.
     function tokenIsAllowed(address _token) public returns (bool) {
         for (uint256 i = 0; i < allowedTokens.length; i++) {
             if (allowedTokens[i] == _token) {
@@ -67,19 +84,22 @@ contract TokenFarm is Ownable {
         return false;
     }
 
-    /// @dev Get the total in USD of tokens staked by a user
-    /// @param _user The user to check
+    /// @dev Get the total in USD of tokens staked by a user.
+    /// @param _user The user to check.
+    /// @return The total in USD of tokens staked by a user.
     function getUserTotalValue(address _user) public view returns (uint256) {
         require(uniqueTokensStaked[_user] > 0, "User has no staked tokens.");
         uint256 totalValue = 0;
         for (uint256 i = 0; i < allowedTokens.length; i++) {
             totalValue = totalValue + stakingBalance[allowedTokens[i]][_user];
         }
+        return totalValue;
     }
 
-    /// @dev Get the value in USD of a token staked by a user
-    /// @param _user The user to check
-    /// @param _token The token to check
+    /// @dev Get the value in USD of a token staked by a user.
+    /// @param _user The user to check.
+    /// @param _token The token to check.
+    /// @return The value in USD of a token staked by a user.
     function getUserSingleTokenValue(address _user, address _token)
         public
         view
@@ -89,37 +109,43 @@ contract TokenFarm is Ownable {
             return 0;
         }
         // get price of token
-        
+        (uint256 price, uint256 decimals) = getTokenValue(_token);
+        return (stakingBalance[_token][_user] * price) / (10**decimals);
     }
 
-    /// @dev Get the USD value of a token
-    /// @param _token The token to check
-    function getTokenValue(address _token) public view returns (uint256) {
-        require(uniqueTokensStaked[_token] > 0, "Token has no staked tokens.");
-        uint256 totalValue = 0;
-        for (uint256 i = 0; i < stakers.length; i++) {
-            totalValue = totalValue + stakingBalance[_token][stakers[i]];
-        }
+    /// @dev Get the USD value of a token.
+    /// @param _token The token to check.
+    /// @return The USD value of a token and the number of decimals the USD value has.
+    function getTokenValue(address _token)
+        public
+        view
+        returns (uint256, uint256)
+    {
+        address priceFeedAddress = tokenToPriceFeed[_token];
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            priceFeedAddress
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        uint256 decimals = priceFeed.decimals();
+        return (uint256(price), uint256(decimals));
     }
 
-    /// @dev Add a token to the list of allowed tokens
-    /// @param _token The token to add
+    /// @dev Add a token to the list of allowed tokens.
+    /// @param _token The token to add.
     function addToken(address _token) public onlyOwner {
         require(_token != address(0));
         require(tokenIsAllowed(_token) == false);
         allowedTokens.push(_token);
     }
 
-    /// @dev Add a user to the uniqueTokensStaked mapping if they are new
-    /// @param _staker Address of the staker
-    /// @param _token Address of the token
+    /// @dev Add a user to the uniqueTokensStaked mapping if they are new.
+    /// @param _user Address of the staker.
+    /// @param _token Address of the token.
     function updateUniqueTokensStaked(address _user, address _token) internal {
         if (stakingBalance[_token][_user] <= 0) {
             uniqueTokensStaked[_user] = uniqueTokensStaked[_user] + 1;
         }
     }
-
-
 
     // get eth value
 }
---
+++ scripts/deploy.py
@@ -0,0 +1,46 @@
+from scripts.utils import get_account, get_contract
+from brownie import DappToken, TokenFarm, config, network
+from web3 import Web3
+
+
+KEEP_BALANCE = Web3.toWei(100, "ether")
+
+
+def main():
+    deploy_token_farm_and_dapp_token()
+
+
+def deploy_token_farm_and_dapp_token():
+    account = get_account()
+    dapp_token = DappToken.deploy({"from": account})
+    token_farm = TokenFarm.deploy(
+        dapp_token.address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()]["verify"],
+    )
+    tx = dapp_token.transfer(
+        token_farm.address, dapp_token.totalSupply() - KEEP_BALANCE, {"from": account}
+    )
+    tx.wait(1)
+
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+    add_allowed_tokens(
+        token_farm,
+        {
+            dapp_token: get_contract("dai_usd_price_feed"),
+            fau_token: get_contract("dai_usd_price_feed"),
+            weth_token: get_contract("eth_usd_price_feed"),
+        },
+        account,
+    )
+    return token_farm, dapp_token
+
+
+def add_allowed_tokens(token_farm, allowed_tokens_map, account):
+    for token, address in allowed_tokens_map.items():
+        tx = token_farm.addToken(token.address, {"from": account})
+        tx.wait(1)
+        tx = token_farm.setPriceFeedContract(token.address, address, {"from": account})
+        tx.wait(1)
+    return token_farm
